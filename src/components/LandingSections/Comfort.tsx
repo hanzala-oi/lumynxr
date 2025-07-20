3a64203bfdae536ece90f62615c343ad327dc08b
ArnavOI <arnav@oneimmersive.us>
2025-07-20 18:59:17 +0530

    navbar links

--- src/components/LandingSections/Comfort.tsx
+++ src/components/LandingSections/Comfort.tsx
@@ -9,41 +9,7 @@ gsap.registerPlugin(ScrollTrigger);
 function Comfort() {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const ctx = gsap.context(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: containerRef.current,
-          start: "top 80%", // when section top hits 80% viewport
-          toggleActions: "play none none none",
-        },
-      });
 
-      // Image animation (parallax-like effect)
-      tl.from(".comfort-image", {
-        scale: 1.1,
-        x: 100,
-        opacity: 0,
-        duration: 1.5,
-        ease: "power3.out",
-      });
-
-      // Heading & Text stagger animation
-      tl.from(
-        ".comfort-text > *",
-        {
-          y: 50,
-          opacity: 0,
-          stagger: 0.2,
-          duration: 1,
-          ease: "power3.out",
-        },
-        "-=1" // start slightly before image finishes
-      );
-    }, containerRef);
-
-    return () => ctx.revert(); // cleanup on unmount
-  }, []);
 
   return (
     <div
@@ -51,7 +17,7 @@ function Comfort() {
       className="flex flex-col md:flex-col-reverse xl:flex-row min-h-[70vh] md:min-h-[85vh] md:max-h-screen md:pt-[100px] xl:pt-0 xl:h-screen w-full bg-black overflow-hidden  "
     >
       {/* Text Block - Relative positioned */}
-      <div className=" comfort-text  z-10 pl-[28px] md:pl-[32px] pt-10 xl:pl-[115px] 2xl:pl-[199px] flex flex-col justify-start  xl:justify-center  mb-10 md:mb-0 h-full  ">
+      <div className=" comfort-textt  z-10 pl-[28px] md:pl-[32px] pt-10 xl:pl-[115px] 2xl:pl-[199px] flex flex-col justify-start  xl:justify-center  mb-10 md:mb-0 h-full  ">
         <h1 className="text-[32px] md:text-[48px] md:leading-[52px] leading-[30px] xl:text-[64px] 2xl:text-[96px] xl:leading-[76px] 2xl:leading-[100px] font-light text-[#E2E2E2]">
           <span className="block md:hidden">Balanced for Comfort</span>
           <span className="hidden md:block">
@@ -82,7 +48,7 @@ function Comfort() {
             alt="Comfort"
             width={600}
             height={400}
-            className="max-w-[250px]  xl:max-w-[400px] 2xl:max-w-[600px]  h-auto object-cover comfort-image xl:ml-[50px]   "
+            className="max-w-[250px]  xl:max-w-[400px] 2xl:max-w-[600px]  h-auto object-cover  xl:ml-[50px]   "
             priority
           />
         </div>
