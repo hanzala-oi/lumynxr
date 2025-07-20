d1615f71d3a2d6a7dc21864732fc2727f2505608
ArnavOI <arnav@oneimmersive.us>
2025-07-20 20:20:04 +0530

    next config changed

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lumynxr-cdn.azureedge.net",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    domains: ['lumynxr-cdn.azureedge.net'],
   },
 };
 
